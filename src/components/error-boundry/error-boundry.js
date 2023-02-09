c53b55884e8a6e28753a88947cef98c6c5059e4b
AndreyGRE <andrey.grenev@mail.ru>
2023-02-09 18:04:55 +0300

    Fir com

---
+++ src/components/error-boundry/error-boundry.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Error from '../error';
+
+export default class ErrorBoundry extends Component {
+
+    state = {
+        error: false
+    }
+
+    componentDidCatch(){
+        this.setState({errror: true})
+    }
+
+    render() {
+       
+       if (this.state.error){
+        return <Error></Error>
+       }
+       return this.props.children;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import {Provider} from 'react-redux'
+import {BrowserRouter 
+    //as  Router
+} from 'react-router-dom'
+import ErrorBoundry from './components/error-boundry'
+import RestoService from './services/resto-service'
+import RestoServiceContext from './components/resto-service-context';
+import  store from './store'
+import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const restoService = new RestoService();
+
+ReactDOM.render(
+    
+    <Provider store={store}>
+        <ErrorBoundry>
+            <RestoServiceContext.Provider value={restoService}>
+            <BrowserRouter>
+                {/* <Router> */}
+                    <App/>
+                {/* </Router> */}
+            </BrowserRouter>
+            </RestoServiceContext.Provider>
+        </ErrorBoundry>
+
+    </Provider>
+    , document.getElementById('root'));
+
